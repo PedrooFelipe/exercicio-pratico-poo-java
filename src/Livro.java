7a380384ea185d83315726a34404e80945869d63
Pedro Felipe <pedro234felipe@gmail.com>
2026-04-09 12:50:46 -0300

    Exercicio Corrigido e feito pelo professor

--- src/Livro.java
+++ src/Livro.java
@@ -13,61 +13,58 @@ public class Livro implements publicacao{
         this.autor = autor;
         this.totPaginas = totpg;
         this.leitor = leitor;
-        setAberto(false);
-        setPaginaAtual(0);
+        this.aberto = false;
+        this.paginaAtual = 0;
 
         System.out.println("================ LEITURA ===============");
         System.out.println(leitor.getNome() + " esta lendo O livro " + getTitulo() + " do autor(a) " + getAutor() + " ele tem " + getTotPaginas() + " paginas");
     }
-    public void detalhes(){
-        System.out.println("=============== DETALHES ================");
-        if(getPaginaAtual() < 0 || getPaginaAtual() > getTotPaginas()){
-            System.out.println("Impossivel esta nessa pagina");
-            setPaginaAtual(0);
-        }
-        System.out.println("o Nome do livro é " + getTitulo() + " é do altor "+ getAutor() +" ele tem " + getTotPaginas() + " paginas voce esta com ele " + getAberto() + " na pagina " + getPaginaAtual());
+    public String detalhes(){
+        return "Livro{" + "Titulo="+ titulo+",\n autor=" + autor+ ",\n total de paginas=" + totPaginas + ",\n pagina atual=" + paginaAtual +",\n aberto=" + aberto + ",\n leitor=" +leitor.getNome() + "}";
     }
 
 
     public void abrir() {
-        if(getAberto()){
+        if(this.aberto){
             System.out.println("Livro ja esta aberto");
         }else {
-            setAberto(true);
+            this.aberto = true;
             System.out.println("Livro aberto");
         }
     }
 
     public void fechar() {
-        if(getAberto() == false){
+        if(this.aberto == false){
             System.out.println("Ja esta fechado");
         }else{
-            setAberto(false);
+            this.aberto = false;
             System.out.println("Livro fechado");
         }
     }
 
-    public void folhear() {
-        Random random = new Random();
-        System.out.println("folheando...");
-        setPaginaAtual(random.nextInt(getTotPaginas()));
+    public void folhear(int p) {
+        if(p > this.totPaginas || p < 0){
+            this.paginaAtual = 0;
+        }else{
+            this.paginaAtual = p;
+        }
     }
 
     public void avancarPag() {
-        if(getPaginaAtual() >= getTotPaginas()){
+        if(this.paginaAtual >= getTotPaginas()){
             System.out.println("imppossivel avançar mais uma pagina");
         }else {
             System.out.println("Passando a página...");
-            setPaginaAtual(getPaginaAtual() + 1);
+            this.paginaAtual++;
         }
     }
 
     public void voltarPag() {
-        if(getPaginaAtual() <= 0){
+        if(this.paginaAtual <= 0){
             System.out.println("Inposivel voltar mais uma pagina");
         }else {
             System.out.println("Voltando a página...");
-            setPaginaAtual(getPaginaAtual() - 1);
+            this.paginaAtual--;
         }
     }
 
@@ -92,22 +89,5 @@ public class Livro implements publicacao{
 
     public void setTotPaginas(int totPaginas) {
         this.totPaginas = totPaginas;
-    }
-
-    public int getPaginaAtual() {
-        return paginaAtual;
-    }
-
-    public void setPaginaAtual(int paginaAtual) {
-            this.paginaAtual = paginaAtual;
-
-    }
-
-    public boolean getAberto() {
-        return aberto;
-    }
-
-    public void setAberto(boolean aberto) {
-        this.aberto = aberto;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        Pessoa[] cara = new Pessoa[5];
+        Pessoa[] cara = new Pessoa[2];
+        Livro[] lv = new Livro[3];
         cara[0] = new Pessoa("Pedro", 20, "Masculino");
+        cara[1] = new Pessoa("Vitoria", 21, "Feminino");
+        lv[0] = new Livro("Trono de Vidro", "Sara J Mass", 550, cara[0]);
+        lv[1] = new Livro("Aprendendo java", "Jose da Silva", 300, cara[1]);
+        lv[2] = new Livro("POO", "Pedro Paulo", 500, cara[0]);
 
-        Livro lv = new Livro("Trono de Vidro", "Sara J Mass", 550, cara[0]);
-        lv.abrir();
-        lv.folhear();
-        lv.detalhes();
-        lv.avancarPag();
-        lv.detalhes();
-        lv.voltarPag();
-        lv.detalhes();
+
+        lv[0].abrir();
+        lv[0].folhear(550);
+        lv[0].voltarPag();
+        System.out.println(lv[0].detalhes());
+
 
     }
 }
